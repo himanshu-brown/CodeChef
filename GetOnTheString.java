3b3b5276f404f0a2bb3c81578e015be679898bef
himanshu-brown <himanshu.brown@gmail.com>
2021-08-24 10:05:04 +0530

    Get on the String

---
+++ GetOnTheString.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class GetOnTheString
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    
+		    InputStreamReader in = new InputStreamReader(System.in);
+		    BufferedReader scan = new BufferedReader(in);
+		    int n = Integer.parseInt(scan.readLine());
+		    String s = scan.readLine();
+		    
+		    System.out.println(s);
+		    
+		}catch(Exception e){
+		    return;
+		}
+	}
+}
+
