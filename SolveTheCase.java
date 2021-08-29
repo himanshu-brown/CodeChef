2f5e51b071e294bfbabf681a9530ad962b83aeff
himanshu-brown <himanshu.brown@gmail.com>
2021-08-29 22:17:08 +0530

    Solve the Case

---
+++ SolveTheCase.java
@@ -0,0 +1,45 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SolveTheCase
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    
+		    Scanner scan = new Scanner(System.in);
+		    int t = scan.nextInt();
+		    
+		    while(t-- != 0){
+		        
+		        int n = scan.nextInt();
+		        int[] a = new int[n];
+		        HashSet<Integer> set = new HashSet<>();
+		        
+		        for(int i=0; i<n; i++)
+		            a[i] = scan.nextInt();
+		            
+		        for(int i=0; i<n; i++)
+		            set.add(a[i]);
+		            
+		        Iterator<Integer> it = set.iterator();
+		        
+		        while(it.hasNext()){
+		            System.out.print(it.next()+" ");
+		        }
+		        
+		        System.out.println();
+		        
+		    }
+		    
+		}catch(Exception e){
+		    return;
+		}
+	}
+}
+
